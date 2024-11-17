dbd60bcc0da83fefb6aaeec2cf78ea0669ece190
3001Wulan <144989232+3001Wulan@users.noreply.github.com>
2024-11-18 02:15:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,7 +7,7 @@ from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 import random
-import os
+
 
 # Daftar gambar banner yang tersedia
 banner_images = ["banner.jpeg", "banner1.jpeg", "banner2.jpeg"]  # Ganti dengan nama gambar Anda
